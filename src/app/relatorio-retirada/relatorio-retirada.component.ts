f01fe487add281f57c73f04a035122d0959dfca6
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2021-11-16 20:31:19 -0300

    systemMedicProd v2.1

--- src/app/relatorio-retirada/relatorio-retirada.component.ts
+++ src/app/relatorio-retirada/relatorio-retirada.component.ts
@@ -140,7 +140,7 @@ export class RelatorioRetiradaComponent implements OnInit {
     });
   }
   
-  async getAllRetirada(){
+  getAllRetirada(){
     this.retiradaSvc.getAllPacientMedicine().subscribe((result)=>{
       this.allRetiradaLst = result;
       this.allRetiradaLst.forEach((retirada) => {
